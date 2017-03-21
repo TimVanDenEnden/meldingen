671cded283c3d53854df81152c462b73149b50c3
STIJN\stijn <99042075@davinci.nl>
2017-03-21 13:05:22 +0100

    Database settings

--- api/includes/settings.php
+++ api/includes/settings.php
@@ -8,10 +8,10 @@
  *
  */
 
-defined("_DB_HOST")				?: define("_DB_HOST",			"t1mdevelopment.ga");
+defined("_DB_HOST")				?: define("_DB_HOST",			"stijndevelopment.ga");
 defined("_DB_USER")				?: define("_DB_USER",			"API");
-defined("_DB_PASSWORD")			?: define("_DB_PASSWORD",		"F3nrOFCVKRJHYTKF");
-defined("_DB_DATABASE")			?: define("_DB_DATABASE",		"meldingen");
+defined("_DB_PASSWORD")			?: define("_DB_PASSWORD",		"CPZ5ETttmt8N0uIv");
+defined("_DB_DATABASE")			?: define("_DB_DATABASE",		"Meldingen");
 defined("_DB_PREFIX")			?: define("_DB_PREFIX",			"Meld_");
 
 defined("_ROOTFOLDER") 			?: define("_ROOTFOLDER",		"");
