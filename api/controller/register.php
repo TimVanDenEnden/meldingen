f3d3b63e179aa3d3b2df60986cf822ffa50ed019
STIJN\stijn <99042075@davinci.nl>
2017-06-01 12:33:20 +0200

    Added register an login systems Edited login class Edited register class Edited Application

--- api/controller/register.php
+++ api/controller/register.php
@@ -2,16 +2,25 @@
 
 class Registration {
 
-    private $db_connection = null;
     public $errors = array();
     public $messages = array();
 
     public function __construct()
     {
-        if (isset($_POST["register"])) {
+        
+    }
+	
+	public function register() {
+		$this->reset();
+		if (isset($_POST["register"])) {
             $this->registerNewUser();
         }
-    }
+	}
+	
+	private function reset() {
+		$errors = array();
+		$messages = array();
+	}
 
     /**
      * handles the entire registration process. checks all error possibilities
@@ -21,22 +30,31 @@ class Registration {
     {
         if (empty($_POST['user_name'])) {
             $this->errors[] = "Empty Username";
+			header($_SERVER["SERVER_PROTOCOL"]." 403 Forbidden");
         } elseif (empty($_POST['user_password_new']) || empty($_POST['user_password_repeat'])) {
             $this->errors[] = "Empty Password";
+			header($_SERVER["SERVER_PROTOCOL"]." 403 Forbidden");
         } elseif ($_POST['user_password_new'] !== $_POST['user_password_repeat']) {
             $this->errors[] = "Password and password repeat are not the same";
+			header($_SERVER["SERVER_PROTOCOL"]." 403 Forbidden");
         } elseif (strlen($_POST['user_password_new']) < 6) {
             $this->errors[] = "Password has a minimum length of 6 characters";
+			header($_SERVER["SERVER_PROTOCOL"]." 403 Forbidden");
         } elseif (strlen($_POST['user_name']) > 64 || strlen($_POST['user_name']) < 2) {
             $this->errors[] = "Username cannot be shorter than 2 or longer than 64 characters";
+			header($_SERVER["SERVER_PROTOCOL"]." 403 Forbidden");
         } elseif (!preg_match('/^[a-z\d]{2,64}$/i', $_POST['user_name'])) {
             $this->errors[] = "Username does not fit the name scheme: only a-Z and numbers are allowed, 2 to 64 characters";
+			header($_SERVER["SERVER_PROTOCOL"]." 403 Forbidden");
         } elseif (empty($_POST['user_email'])) {
             $this->errors[] = "Email cannot be empty";
+			header($_SERVER["SERVER_PROTOCOL"]." 403 Forbidden");
         } elseif (strlen($_POST['user_email']) > 64) {
             $this->errors[] = "Email cannot be longer than 64 characters";
+			header($_SERVER["SERVER_PROTOCOL"]." 403 Forbidden");
         } elseif (!filter_var($_POST['user_email'], FILTER_VALIDATE_EMAIL)) {
             $this->errors[] = "Your email address is not in a valid email format";
+			header($_SERVER["SERVER_PROTOCOL"]." 403 Forbidden");
         } elseif (!empty($_POST['user_name'])
             && strlen($_POST['user_name']) <= 64
             && strlen($_POST['user_name']) >= 2
@@ -80,6 +98,7 @@ class Registration {
 
                 if ($query_check_user_name->num_rows == 1) {
                     $this->errors[] = "Sorry, that username / email address is already taken.";
+					header($_SERVER["SERVER_PROTOCOL"]." 403 Forbidden");
                 } else {
                     // write new user's data into database
                     $sql = "INSERT INTO "._DB_PREFIX."login_users (user_name, user_password_hash, user_email) VALUES(?, ?, ?);";
@@ -94,15 +113,19 @@ class Registration {
                     // if user has been added successfully
                     if ($query_new_user_insert) {
                         $this->messages[] = "Your account has been created successfully. You can now log in.";
+						header($_SERVER["SERVER_PROTOCOL"]." 200 OK");
                     } else {
                         $this->errors[] = "Sorry, your registration failed. Please go back and try again.";
+						header($_SERVER["SERVER_PROTOCOL"]." 403 Forbidden");
                     }
                 }
             } else {
                 $this->errors[] = "Sorry, no database connection.";
+				header($_SERVER["SERVER_PROTOCOL"]." 500 Internal Server Error");
             }
         } else {
             $this->errors[] = "An unknown error occurred.";
+			header($_SERVER["SERVER_PROTOCOL"]." 500 Internal Server Error");
         }
     }
 }
